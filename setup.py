41e09547a614a0a272c79aa053855020b187f3d0
Damon Lynch <damonlynch@gmail.com>
2009-03-25 18:36:57 +0800

    packaging fixes

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 
 from distutils.core import setup
 from rapid.rapid import __version__ as version
+import glob
 
-package_data={'rapid': ['glade3/rapid.glade',  'glade3/rapid-photo-downloader-icon.png',]}
+package_data={'rapid': ['glade3/rapid.glade', 'glade3/rapid-photo-downloader-about.png']}
 
 setup(name='rapid-photo-downloader',
       version=version,
@@ -14,9 +15,24 @@ setup(name='rapid-photo-downloader',
       author_email='damonlynch@gmail.com',
       maintainer='Damon Lynch',
       url='http://www.damonlynch.net/rapid',
-      long_description='Rapid Photo Downloader is written by a photographer for professional and amateur photographers, designed for use on the GNOME 2 Desktop. It can download photos from multiple memory cards and Portable Storage Devices simultaneously. It provides a variety of options for subfolder creation, image renaming and backup.',
+      long_description=
+"""Rapid Photo Downloader is written by a photographer for
+professional and amateur photographers, designed for use
+on the GNOME 2 Desktop. It can download photos from multiple
+memory cards and Portable Storage Devices simultaneously. It
+provides a variety of options for subfolder creation, image
+renaming and backup.""",
       packages = ['rapid'], 
       package_data=package_data, 
       scripts=['rapid-photo-downloader'],
       platforms=['linux'],
+      data_files=[
+                  ('share/applications', ['data/rapid-photo-downloader.desktop']),
+                  ('share/pixmaps', ['data/icons/48x48/apps/rapid-photo-downloader.png', 'data/icons/rapid-photo-downloader.xpm']),
+                  ('share/icons/hicolor/scalable/apps', glob.glob('data/icons/scalable/apps/*.svg')),
+                  ('share/icons/hicolor/16x16/apps', glob.glob('data/icons/16x16/apps/*.png')),
+                  ('share/icons/hicolor/22x22/apps', glob.glob('data/icons/22x22/apps/*.png')),
+                  ('share/icons/hicolor/24x24/apps', glob.glob('data/icons/24x24/apps/*.png')),
+                  ('share/icons/hicolor/48x48/apps', glob.glob('data/icons/48x48/apps/*.png')),
+                 ],      
      )
