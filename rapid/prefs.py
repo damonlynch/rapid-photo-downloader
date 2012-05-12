d6af3b0948a2eff0d041126239b2e330be8afa8b
Damon Lynch <damonlynch@gmail.com>
2012-05-12 00:33:21 -0500

    Fixed bug #996613: Updated Free Software Foundation address.

--- rapid/prefs.py
+++ rapid/prefs.py
@@ -12,7 +12,8 @@
 
 ### You should have received a copy of the GNU General Public License
 ### along with this program; if not, write to the Free Software
-### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+### USA
 
 ### Modified August 2007 by Damon Lynch to allow use of list value preferences
 ### Modified May 2010 by Damon Lynch to allow preferences to be reset
