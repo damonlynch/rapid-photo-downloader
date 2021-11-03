9609d7addfd12c9a66e2a020c22eaa478d334320
Damon Lynch <damonlynch@gmail.com>
2021-11-03 17:30:34 -0400

    Use Windows registry values for Pictures and Videos user folders

--- raphodo/storage.py
+++ raphodo/storage.py
@@ -49,6 +49,7 @@ __copyright__ = (
 )
 
 import logging
+import functools
 import os
 import re
 import sys
@@ -56,7 +57,7 @@ import time
 import subprocess
 import shlex
 import pwd
-from pathlib import Path
+from pathlib import Path, PureWindowsPath
 import shutil
 from collections import namedtuple
 from typing import Optional, Tuple, List, Dict, Set
@@ -74,6 +75,7 @@ from PyQt5.QtCore import (
     QStandardPaths,
 )
 from showinfm import linux_desktop, LinuxDesktop, valid_file_manager
+from showinfm.system.linux import translate_wsl_path
 
 import gi
 
@@ -421,6 +423,76 @@ def get_desktop_environment() -> Optional[str]:
     return os.getenv("XDG_CURRENT_DESKTOP")
 
 
+@functools.lru_cache(maxsize=None)
+def wsl_env_variable(variable: str) -> str:
+    """
+    Return Windows environment variable within WSL
+    """
+
+    assert variable
+    return subprocess.run(
+        shlex.split(f"wslvar {variable}"),
+        universal_newlines=True,
+        capture_output=True,
+    ).stdout.strip()
+
+
+@functools.lru_cache(maxsize=None)
+def wsl_home() -> Path:
+    """
+    Return user's Windows home directory within WSL
+    """
+
+    win_path = shlex.quote(wsl_env_variable("USERPROFILE"))
+    return Path(translate_wsl_path(win_path, from_windows_to_wsl=True))
+
+
+@functools.lru_cache(maxsize=None)
+def _wsl_reg_query_standard_folder(folder: str) -> str:
+    """
+    Use reg query on Windows to query the user's Pictures and Videos folder.
+
+    No error checking.
+
+    :param folder: one of "My Pictures" or "My Video"
+    :return: registry value for the folder
+    """
+
+    assert folder in ("My Pictures", "My Video")
+    query = fr"reg.exe query 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders\' /v '{folder}'"
+    output = subprocess.run(
+        shlex.split(query),
+        capture_output=True,
+        universal_newlines=True,
+    ).stdout
+    regex = rf"{folder}\s+REG_EXPAND_SZ\s+(.+)\n\n$"
+    return re.search(regex, output).group(1)
+
+
+@functools.lru_cache(maxsize=None)
+def wsl_pictures_folder() -> str:
+    """
+    Query the Windows registry for the location of the user's Pictures folder
+    :return: location as a Linux path
+    """
+
+    return translate_wsl_path(
+        _wsl_reg_query_standard_folder("My Pictures"), from_windows_to_wsl=True
+    )
+
+
+@functools.lru_cache(maxsize=None)
+def wsl_videos_folder() -> str:
+    """
+    Query the Windows registry for the location of the user's Videos folder
+    :return: location as a Linux path
+    """
+
+    return translate_wsl_path(
+        _wsl_reg_query_standard_folder("My Video"), from_windows_to_wsl=True
+    )
+
+
 def _platform_special_dir(
     dir_type: QStandardPaths, home_on_failure: bool = True
 ) -> Optional[str]:
@@ -428,7 +500,8 @@ def _platform_special_dir(
     Use Qt to query the platforms standard paths
 
     :param dir_type: one of Qt's standard paths
-    :param home_on_failure: return the home directory if the special path cannot be located
+    :param home_on_failure: return the home directory if the special path cannot
+     be located
     :return: the directory, or None if it cannot be determined
     """
 
@@ -452,6 +525,15 @@ def platform_photos_directory(home_on_failure: bool = True) -> Optional[str]:
     :return: the directory if it is specified, else the user's
     home directory or None
     """
+
+    if linux_desktop() == LinuxDesktop.wsl2:
+        try:
+            return wsl_pictures_folder()
+        except Exception as e:
+            logging.error("Error querying Windows registry: %s", str(e))
+        path = wsl_home()
+        if path.is_dir():
+            return str(path / "Pictures")
     return _platform_special_dir(QStandardPaths.PicturesLocation, home_on_failure)
 
 
@@ -464,6 +546,15 @@ def platform_videos_directory(home_on_failure: bool = True) -> str:
     :return: the directory if it is specified, else the user's
     home directory or None
     """
+
+    if linux_desktop() == LinuxDesktop.wsl2:
+        try:
+            return wsl_videos_folder()
+        except Exception as e:
+            logging.error("Error querying Windows registry: %s", str(e))
+        path = wsl_home()
+        if path.is_dir():
+            return str(path / "Videos")
     return _platform_special_dir(QStandardPaths.MoviesLocation, home_on_failure)
 
 
@@ -587,7 +678,9 @@ def get_program_data_directory(create_if_not_exist=False) -> Optional[str]:
     :return: the full path of the data directory, or None on error
     """
 
-    data_directory = _platform_special_dir(QStandardPaths.GenericDataLocation, home_on_failure=False)
+    data_directory = _platform_special_dir(
+        QStandardPaths.GenericDataLocation, home_on_failure=False
+    )
     if data_directory is None:
         logging.error("The program's data directory could not be determined")
         return None
