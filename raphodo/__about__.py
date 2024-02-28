9a513ada42187b17dcba32686ccad742363413c2
Damon Lynch <damonlynch@gmail.com>
2024-02-29 09:02:52 +1300

    Enhance cmd line --help; delete man pod

--- raphodo/__about__.py
+++ raphodo/__about__.py
@@ -33,7 +33,7 @@ __all__ = [
 __title__ = "rapid-photo-downloader"
 __summary__ = (
     "Downloads, renames and backs up photos and videos from cameras, phones, "
-    "memory cards and other devices"
+    "memory cards and other devices."
 )
 __uri__ = "https://damonlynch.net/rapid"
 
@@ -43,4 +43,25 @@ __author__ = "Damon Lynch"
 __email__ = "damonlynch@gmail.com"
 
 __license__ = "GPL 3+"
+__licence_full__ = (
+    "License GPLv3+: GNU GPL version 3 or later https://gnu.org/licenses/gpl.html.\n"
+    "This is free software: you are free to change and redistribute it. "
+    "There is NO WARRANTY, to the extent permitted by law."
+)
 __copyright__ = f"Copyright 2007-2024 {__author__}"
+
+__help_epilog__ = (
+    "If the environment variable RPD_SCAN_DEBUG is set to any value, the program's "
+    "scan operation will output voluminous debug information to stdout.\n\n"
+
+    "If the environment variable RPD_DEVEL_DEFAULTS is set to any value, the "
+    "program's default preferences will be set to those of a development "
+    "environment.\n\n"
+    
+    "Report bugs to https://bugs.rapidphotodownloader.com/\n\n"
+    
+    f"Written by {__author__} {__email__}.\n\n"
+    
+    f"{__copyright__}. {__licence_full__}\n\n"
+    ""
+)
--- raphodo/rapid.py
+++ raphodo/rapid.py
@@ -45,7 +45,6 @@ with contextlib.suppress(locale.Error):
     # Use the default locale as defined by the LANG variable
     locale.setlocale(locale.LC_ALL, "")
 
-import argparse
 import faulthandler
 import functools
 import importlib.metadata
@@ -147,6 +146,7 @@ import raphodo.metadata.fileformats as fileformats
 import raphodo.storage.storageidevice as storageidevice
 import raphodo.ui.didyouknow as didyouknow
 import raphodo.tools.utilities
+from raphodo.argumentsparse import get_parser
 from raphodo.cache import ThumbnailCacheSql
 from raphodo.camera import (
     autodetect_cameras,
@@ -6588,247 +6588,6 @@ def get_versions(
     return versions
 
 
-def parser_options(formatter_class=argparse.HelpFormatter):
-    parser = argparse.ArgumentParser(
-        prog=__about__.__title__,
-        description=__about__.__summary__,
-        formatter_class=formatter_class,
-    )
-
-    parser.add_argument(
-        "--version",
-        action="version",
-        version=f"%(prog)s {__about__.__version__}",
-    )
-    parser.add_argument(
-        "--detailed-version",
-        action="store_true",
-        help=_("Show version numbers of program and its libraries and exit."),
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        dest="verbose",
-        help=_("Display program information when run from the command line."),
-    )
-    parser.add_argument(
-        "--debug",
-        action="store_true",
-        dest="debug",
-        help=_("Display debugging information when run from the command line."),
-    )
-    parser.add_argument(
-        "-e",
-        "--extensions",
-        action="store_true",
-        dest="extensions",
-        help=_("List photo and video file extensions the program recognizes and exit."),
-    )
-    parser.add_argument(
-        "--photo-renaming",
-        choices=["on", "off"],
-        dest="photo_renaming",
-        help=_("Turn on or off the the renaming of photos."),
-    )
-    parser.add_argument(
-        "--video-renaming",
-        choices=["on", "off"],
-        dest="video_renaming",
-        help=_("Turn on or off the the renaming of videos."),
-    )
-    parser.add_argument(
-        "-a",
-        "--auto-detect",
-        choices=["on", "off"],
-        dest="auto_detect",
-        help=_(
-            "Turn on or off the automatic detection of devices from which to download."
-        ),
-    )
-    parser.add_argument(
-        "-t",
-        "--this-computer",
-        choices=["on", "off"],
-        dest="this_computer_source",
-        help=_("Turn on or off downloading from this computer."),
-    )
-    parser.add_argument(
-        "--this-computer-location",
-        type=str,
-        metavar=_("PATH"),
-        dest="this_computer_location",
-        help=_("The PATH on this computer from which to download."),
-    )
-    parser.add_argument(
-        "--photo-destination",
-        type=str,
-        metavar=_("PATH"),
-        dest="photo_location",
-        help=_("The PATH where photos will be downloaded to."),
-    )
-    parser.add_argument(
-        "--video-destination",
-        type=str,
-        metavar=_("PATH"),
-        dest="video_location",
-        help=_("The PATH where videos will be downloaded to."),
-    )
-    parser.add_argument(
-        "-b",
-        "--backup",
-        choices=["on", "off"],
-        dest="backup",
-        help=_("Turn on or off the backing up of photos and videos while downloading."),
-    )
-    parser.add_argument(
-        "--backup-auto-detect",
-        choices=["on", "off"],
-        dest="backup_auto_detect",
-        help=_("Turn on or off the automatic detection of backup devices."),
-    )
-    parser.add_argument(
-        "--photo-backup-identifier",
-        type=str,
-        metavar=_("FOLDER"),
-        dest="photo_backup_identifier",
-        help=_(
-            "The FOLDER in which backups are stored on the automatically detected "
-            "photo backup device, with the folder's name being used to identify "
-            "whether or not the device is used for backups. For each device you wish "
-            "to use for backing photos up to, create a folder on it with this name."
-        ),
-    )
-    parser.add_argument(
-        "--video-backup-identifier",
-        type=str,
-        metavar=_("FOLDER"),
-        dest="video_backup_identifier",
-        help=_(
-            "The FOLDER in which backups are stored on the automatically detected "
-            "video backup device, with the folder's name being used to identify "
-            "whether or not the device is used for backups. For each device you wish "
-            "to use for backing up videos to, create a folder on it with this name."
-        ),
-    )
-    parser.add_argument(
-        "--photo-backup-location",
-        type=str,
-        metavar=_("PATH"),
-        dest="photo_backup_location",
-        help=_(
-            "The PATH where photos will be backed up when automatic detection of "
-            "backup devices is turned off."
-        ),
-    )
-    parser.add_argument(
-        "--video-backup-location",
-        type=str,
-        metavar=_("PATH"),
-        dest="video_backup_location",
-        help=_(
-            "The PATH where videos will be backed up when automatic detection of "
-            "backup devices is turned off."
-        ),
-    )
-    parser.add_argument(
-        "--ignore-other-photo-file-types",
-        action="store_true",
-        dest="ignore_other",
-        help=_("Ignore photos with the following extensions: %s")
-        % make_internationalized_list(
-            [s.upper() for s in fileformats.OTHER_PHOTO_EXTENSIONS]
-        ),
-    )
-    parser.add_argument(
-        "--auto-download-startup",
-        dest="auto_download_startup",
-        choices=["on", "off"],
-        help=_(
-            "Turn on or off starting downloads as soon as the program itself starts."
-        ),
-    )
-    parser.add_argument(
-        "--auto-download-device-insertion",
-        dest="auto_download_insertion",
-        choices=["on", "off"],
-        help=_("Turn on or off starting downloads as soon as a device is inserted."),
-    )
-    parser.add_argument(
-        "--thumbnail-cache",
-        dest="thumb_cache",
-        choices=["on", "off"],
-        help=_(
-            "Turn on or off use of the Rapid Photo Downloader Thumbnail Cache. "
-            "Turning it off does not delete existing cache contents."
-        ),
-    )
-    parser.add_argument(
-        "--delete-thumbnail-cache",
-        dest="delete_thumb_cache",
-        action="store_true",
-        help=_(
-            "Delete all thumbnails in the Rapid Photo Downloader Thumbnail Cache, "
-            "and exit."
-        ),
-    )
-    parser.add_argument(
-        "--forget-remembered-files",
-        dest="forget_files",
-        action="store_true",
-        help=_("Forget which files have been previously downloaded, and exit."),
-    )
-    parser.add_argument(
-        "--import-old-version-preferences",
-        action="store_true",
-        dest="import_prefs",
-        help=_(
-            "Import preferences from an old program version and exit. Requires the "
-            "command line program gconftool-2."
-        ),
-    )
-    parser.add_argument(
-        "--reset",
-        action="store_true",
-        dest="reset",
-        help=_(
-            "Reset all program settings to their default values, delete all thumbnails "
-            "in the Thumbnail cache, forget which files have been previously "
-            "downloaded, and exit."
-        ),
-    )
-    parser.add_argument(
-        "--log-gphoto2",
-        action="store_true",
-        help=_("Include gphoto2 debugging information in log files."),
-    )
-
-    parser.add_argument(
-        "--camera-info",
-        action="store_true",
-        help=_("Print information to the terminal about attached cameras and exit."),
-    )
-
-    parser.add_argument(
-        "--force-system-theme",
-        action="store_true",
-        default=False,
-        help=_("Use the system Qt theme instead of the built-in theme"),
-    )
-
-    parser.add_argument("path", nargs="?")
-
-    if platform.system() == "Linux":
-        parser.add_argument(
-            "-platform",
-            type=str,
-            choices=["wayland", "xcb"],
-            help=_("Run this program in wayland or regular X11"),
-        )
-
-    return parser
-
-
 def import_prefs() -> None:
     """
     Import program preferences from the Gtk+ 2 version of the program.
@@ -7030,7 +6789,7 @@ def main():
     # Must parse args before calling QApplication:
     # Calling QApplication.setAttribute below causes QApplication to parse sys.argv
 
-    parser = parser_options()
+    parser = get_parser()
     args = parser.parse_args()
 
     try:
@@ -7422,8 +7181,7 @@ def main():
             if count:
                 d.update_table(reset=True)
             print(
-                _("%(count)s remembered files have been forgotten.")
-                % dict(count=count)
+                _("%(count)s remembered files have been forgotten.") % dict(count=count)
             )
             logging.debug("%s remembered files have been forgotten", count)
 
