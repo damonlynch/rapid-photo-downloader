d4db5d12ac68780219c8d4a41579315b6983eba2
Damon Lynch <damonlynch@gmail.com>
2026-02-01 13:30:43 +1300

    Use cmd line to retrieve Gnome settings accent colour
    Using the Python wrapper gi.Gio.Settings.get_string will crash if
    accent-color is specified on Gnome where Gnome is not a very recent
    release. Instead, use the command line to attempt to retrieve the value,
    falling back to the theme if not found.

--- raphodo/tools/gnomeplatform.py
+++ raphodo/tools/gnomeplatform.py
@@ -1,6 +1,10 @@
 #  SPDX-FileCopyrightText: 2026 Damon Lynch <damonlynch@gmail.com>
 #  SPDX-License-Identifier: GPL-3.0-or-later
 
+
+import shlex
+from subprocess import DEVNULL, CalledProcessError, check_output
+
 import gi
 
 try:
@@ -12,23 +16,47 @@ except Exception:
     have_gio = False
 
 
-def get_gnome_color_scheme() -> str:
-    """Return the Gnome color scheme from gsettings as a string"""
+def get_gnome_setting_subprocess(setting: str) -> str:
+    cmd = f"gsettings get org.gnome.desktop.interface {setting}"
+    shlex.split(cmd)
+    try:
+        return (
+            check_output(shlex.split(cmd), text=True, stderr=DEVNULL)
+            .strip()
+            .strip("'")
+            .lower()
+        )
+    except CalledProcessError:
+        return ""
+
+
+def get_gnome_setting(setting: str) -> str:
+    """Return the Gnome setting from gsettings as a string"""
     if not have_gio:
-        raise Exception("Gnome color scheme not available")
+        raise Exception("Gnome scheme not available")
 
     schema_id = "org.gnome.desktop.interface"
 
     # Note: This will crash (segfault) if the schema is not installed on the system
     settings = Gio.Settings.new(schema_id)
 
-    color_scheme = settings.get_string("color-scheme")
-    return color_scheme
+    return settings.get_string(setting)
 
 
 def gnome_prefer_dark() -> bool:
     """Return true if Gnome is set to run in dark mode"""
     try:
-        return get_gnome_color_scheme() == "prefer-dark"
+        return get_gnome_setting(setting="color-scheme") == "prefer-dark"
     except Exception:
         return False
+
+
+def gnome_accent_color() -> str:
+    """Return Gnome accent color or gtk-theme"""
+    return get_gnome_setting_subprocess("accent-color") or get_gnome_setting_subprocess(
+        "gtk-theme"
+    )
+
+
+if __name__ == "__main__":
+    print(gnome_accent_color())
