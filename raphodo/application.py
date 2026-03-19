652f9f36827f77416670346474191342d1f878e9
Damon Lynch <damonlynch@gmail.com>
2026-03-19 17:23:56 +1300

    Get accent color on older versions of Gnome 3
    Get Gnome specific accent color setting, i.e. where
    "org.freedesktop.appearance" is not used, but
    "org.gnome.desktop.interface" is.

--- raphodo/application.py
+++ raphodo/application.py
@@ -1,6 +1,7 @@
 #  SPDX-FileCopyrightText: 2026 Damon Lynch <damonlynch@gmail.com>
 #  SPDX-License-Identifier: GPL-3.0-or-later
 
+import logging
 from typing import Any
 
 from PyQt6.QtCore import QEvent, pyqtSignal, pyqtSlot
@@ -10,6 +11,8 @@ from PyQt6.QtGui import QColor, QPalette
 from raphodo.constants import StockAccent
 from raphodo.qtsingleapplication import QtSingleApplication
 from raphodo.ui.applicationpalette import darkPalette, standardPalette
+from raphodo.ui.gnomepalette import GnomeAccentColor
+from raphodo.ui.uipalette import listViewBorderColor
 
 
 class Application(QtSingleApplication):
@@ -22,12 +25,14 @@ class Application(QtSingleApplication):
 
     def __init__(self, programId: str, *argv) -> None:
         super().__init__(programId, *argv)
+        self._log_accent = False
         self._dark_mode = False
         self._accentColor = QColor(StockAccent)
 
         self._raw_color_scheme: int = 2
         # tuple[float, float, float]
-        self._raw_accent_colour: tuple = self._accentColor.getRgbF()[:3]
+        self._raw_accent_colour: tuple | None = None
+        self._raw_gtk_theme: str | None
 
         bus = QDBusConnection.sessionBus()
         if not bus.isConnected():
@@ -48,6 +53,7 @@ class Application(QtSingleApplication):
 
     def event(self, event: QEvent) -> bool:
         if event.type() == QEvent.Type.ApplicationPaletteChange:
+            listViewBorderColor.cache_clear()
             self.applicationPaletteChanged.emit(self.darkMode)
         return super().event(event)
 
@@ -69,6 +75,12 @@ class Application(QtSingleApplication):
 
     @accentColor.setter
     def accentColor(self, accentColor: QColor) -> None:
+        # Don't log the accent color during start-up
+        if self._log_accent:
+            logging.debug("Setting accent color to %s", accentColor.name())
+        else:
+            self._log_accent = True
+
         self._accentColor = accentColor
         palette = self.palette()
         palette.setColor(
@@ -81,24 +93,33 @@ class Application(QtSingleApplication):
 
     @pyqtSlot(str, str, QDBusVariant)
     def DBusSettingChanged(self, namespace, key, dbus_variant):
-        if namespace == "org.freedesktop.appearance":
-            match key:
-                case "color-scheme":
-                    color_scheme = dbus_variant.variant()
-                    if color_scheme == self._raw_color_scheme:
-                        return
-                    self._raw_color_scheme = color_scheme
-                    self._setDarkMode()
-                case "accent-color":
-                    raw_color = dbus_variant.variant()
-                    if self._raw_accent_colour == raw_color:
-                        return
-                    self._raw_accent_colour = raw_color[:3]
-                    self._setAccentColor()
+        match namespace:
+            case "org.freedesktop.appearance":
+                match key:
+                    case "color-scheme":
+                        color_scheme = dbus_variant.variant()
+                        if color_scheme == self._raw_color_scheme:
+                            return
+                        self._raw_color_scheme = color_scheme
+                        self._setDarkMode()
+                    case "accent-color":
+                        raw_color = dbus_variant.variant()
+                        if self._raw_accent_colour == raw_color:
+                            return
+                        self._raw_accent_colour = raw_color[:3]
+                        self._setAccentColor()
+            case "org.gnome.desktop.interface":
+                match key:
+                    case "gtk-theme":
+                        color_identifier = dbus_variant.variant()
+                        if color_identifier == self._raw_gtk_theme:
+                            return
+                        self._raw_gtk_theme = color_identifier
+                        self._setThemeColor()
 
     @staticmethod
-    def _callDBus(iface, key) -> Any:
-        msg = iface.call("Read", "org.freedesktop.appearance", key)
+    def _callDBus(iface, namespace, key) -> Any:
+        msg = iface.call("Read", namespace, key)
 
         if not msg.errorName():
             args = msg.arguments()
@@ -118,11 +139,21 @@ class Application(QtSingleApplication):
             QDBusConnection.sessionBus(),
         )
 
-        self._raw_color_scheme = self._callDBus(iface=iface, key="color-scheme")
-        self._raw_accent_colour = self._callDBus(iface=iface, key="accent-color")
+        self._raw_color_scheme = self._callDBus(
+            iface=iface, namespace="org.freedesktop.appearance", key="color-scheme"
+        )
+        self._raw_accent_colour = self._callDBus(
+            iface=iface, namespace="org.freedesktop.appearance", key="accent-color"
+        )
+        self._raw_gtk_theme = self._callDBus(
+            iface=iface, namespace="org.gnome.desktop.interface", key="gtk-theme"
+        )
 
         self._setDarkMode()
-        self._setAccentColor()
+        if self._raw_accent_colour is not None or self._raw_gtk_theme is None:
+            self._setAccentColor()
+        elif self._raw_gtk_theme:
+            self._setThemeColor()
 
     def _setDarkMode(self) -> None:
         self.darkMode = self._raw_color_scheme == 1
@@ -131,4 +162,13 @@ class Application(QtSingleApplication):
         if self._raw_accent_colour is not None:
             self.accentColor = QColor.fromRgbF(*self._raw_accent_colour)
         else:
+            logging.debug("Setting stock accent color", self._raw_accent_colour)
             self.accentColor = QColor(StockAccent)
+
+    def _setThemeColor(self) -> None:
+        if self._raw_accent_colour is not None:
+            return
+        color = QColor(GnomeAccentColor[self._raw_gtk_theme.lower().strip()])
+        if color != self.accentColor:
+            logging.debug("Setting accent color using GTK theme")
+            self.accentColor = color
