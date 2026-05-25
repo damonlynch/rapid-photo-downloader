4bd582872f5906cacedeaa2fe4d11a78f52100b6
Damon Lynch <damonlynch@gmail.com>
2026-05-25 16:13:23 +1200

    Bump dev version

--- raphodo/__about__.py
+++ raphodo/__about__.py
@@ -19,7 +19,7 @@ __summary__ = (
 )
 __uri__ = "https://damonlynch.net/rapid"
 
-__version__ = "0.9.38a1.dev1"
+__version__ = "0.9.38a1.dev3"
 
 __author__ = "Damon Lynch"
 __email__ = "damonlynch@gmail.com"
